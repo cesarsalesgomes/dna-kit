1416942ed9b97f807c7dfda710ee52ca6c3c20fb
Cesar <cesarsalesgomes@gmail.com>
2024-04-28 11:37:23 -0300

    Necessary changes to run Sveltekit with Bun locally and in production

--- sveltekit/svelte.config.js
+++ sveltekit/svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from 'svelte-adapter-bun';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,6 +12,25 @@ const config = {
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
+    alias: {
+      '$assets/*': 'src/assets/*',
+      '$components/*': 'src/components/*',
+      '$config/*': 'src/config/*',
+      '$constants/*': 'src/constants/*',
+      '$enums/*': 'src/enums/*',
+      '$features/*': 'src/features/*',
+      '$hooks/*': 'src/hooks/*',
+      '$interfaces/*': 'src/interfaces/*',
+      $lib: 'src/lib',
+      '$lib/*': 'src/lib/*',
+      '$providers/*': 'src/providers/*',
+      '$repository/*': 'src/repository/*',
+      '$routes/*': 'src/routes/*',
+      '$stores/*': 'src/stores/*',
+      '$test/*': 'src/test/*',
+      '$types/*': 'src/types/*',
+      '$utils/*': 'src/utils/*',
+    },
     prerender: {
       entries: ['/(protected)/cats', '/(protected)/birds'],
     },
